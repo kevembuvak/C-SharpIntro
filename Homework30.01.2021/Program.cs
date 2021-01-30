c7516e4c95e5db02620f060901da63c55bc3c587
Kerem Burak Yılmaz <k.burak02@outlook.com>
2021-01-30 17:09:20 +0300

    Homework

---
+++ Homework30.01.2021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework30._01._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //screenshot from Gökhan Çam, https://github.com/gokcam
+        }
+    }
+}
